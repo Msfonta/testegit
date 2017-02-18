ecc4f4d583d64b4d930702774f0933ce77099f13
Marco Senna Fontanari <marcosennafontanari@gmail.com>
2017-02-18 12:28:29 -0200

    Adicionando classe Usuario a estrutura

---
+++ Usuario.java
@@ -0,0 +1,9 @@
+public class Usuario extends Pessoa{
+	String empresa;
+	String email;
+
+	public class Usuario(String empresa, String email){
+		this.empresa = empresa;
+		this.email = email;
+	}
+}

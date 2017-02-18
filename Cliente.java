e2b2b37191f2570cbf7a2bc20942150f557639d0
Marco Senna Fontanari <marcosennafontanari@gmail.com>
2017-02-18 19:20:07 -0200

    Adicionando classe cliente a estrutura

---
+++ Cliente.java
@@ -0,0 +1,9 @@
+public class Cliente extends Usuario {
+	private String login;
+	private String senha;
+
+	public Cliente(String login, String senha){
+		this.login = login;
+		this.senha = senha;
+	}
+}
